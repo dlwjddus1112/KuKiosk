032a2506f757747963a415e33c52da67b1d0bacd
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2024-09-17 21:49:25 +0900

    Update ProductRepository.java

--- src/repository/ProductRepository.java
+++ src/repository/ProductRepository.java
@@ -8,7 +8,5 @@ import java.util.List;
 public class ProductRepository {
     List<Product> products = new ArrayList<Product>();
 
-    void addProduct(String productName, int price, int maxCount ) {
-      products.add(new Product(productName, price, maxCount));
-    }
+    
 }
